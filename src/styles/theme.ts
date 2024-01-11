189b6d687cab10dbc71726dc55d1bed25184b1a4
hiroyuki-higashio <tonbi0521@gmail.com>
2024-01-11 21:24:59 +0900

    global styleの設定

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export enum Color {
+  Main = '#29b6f6',
+  MainDark = '#1c7fac',
+  MainLight = '#53c4f7',
+  Border = '#e3e3e3',
+  White = '#ffffff',
+  Black = '#3c3c3c',
+  Gray = '#8a8a8a',
+  Red = '#ff4342',
+}
